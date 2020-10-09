395fd1f1a8cbfbd7061819d20e047edca303e904
Osenias Oliveira <oseniasjunior@gmail.com>
2020-10-08 20:35:24 -0400

    Add novos translates

--- core/models.py
+++ core/models.py
@@ -39,85 +39,113 @@ class MeansPayment(ModelBase):
         max_length=104,
         null=False,
         unique=True,
-        verbose_name=_('Description')
+        verbose_name=_('Description'),
+        error_messages={'unique': _('Means payment already exists')}
     )
 
     class Meta:
         db_table = 'means_payment'
         verbose_name = _('Means payment')
-        verbose_name_plural = _('Means payment')
+        verbose_name_plural = _('Means payments')
+
+    def __str__(self):
+        return self.description
 
 
 class Table(ModelBase):
     class Status(models.TextChoices):
-        OCUPPED = 'O', 'Occuped'
-        DESOCCUPED = 'D', 'Desoccuped'
+        OCUPPED = 'O', _('Occuped')
+        DESOCCUPED = 'D', _('Desoccuped')
 
     number = models.IntegerField(
         null=False,
-        unique=True
+        unique=True,
+        verbose_name=_('Number')
     )
     status = models.CharField(
         max_length=1,
         default=Status.DESOCCUPED,
-        choices=Status.choices
+        choices=Status.choices,
+        verbose_name=_('Status')
     )
 
     class Meta:
         db_table = 'table'
+        verbose_name = _('Table')
+        verbose_name_plural = _('Tables')
+
+    def __str__(self):
+        return str(self.number)
 
 
 class Product(ModelBase):
     name = models.CharField(
         max_length=104,
-        null=False
+        null=False,
+        verbose_name=_('Name')
     )
     detail_description = models.TextField(
         null=True,
-        blank=True
+        blank=True,
+        verbose_name=_('Detail description')
     )
     sale_price = models.DecimalField(
         null=False,
         max_digits=10,
-        decimal_places=2
+        decimal_places=2,
+        verbose_name=_('Sale price')
     )
 
     class Meta:
         db_table = 'product'
+        verbose_name = _('Product')
+        verbose_name_plural = _('Products')
+
+    def __str__(self):
+        return self.name
 
 
 class Order(ModelBase):
     class Status(models.TextChoices):
-        OPENED = 'O', 'Opened'
-        CLOSED = 'C', 'Closed'
-        CANCELED = 'X', 'Canceled'
-        PARTIAL_CLOSED = 'P', 'Partial closed'
+        OPENED = 'O', _('Opened')
+        CLOSED = 'C', _('Closed')
+        CANCELED = 'X', _('Canceled')
+        PARTIAL_CLOSED = 'P', _('Partial closed')
 
     date = models.DateTimeField(
         null=False,
-        auto_now_add=True
+        auto_now_add=True,
+        verbose_name=_('Date')
     )
     table = models.ForeignKey(
         to='Table',
         on_delete=models.DO_NOTHING,
         db_column='id_table',
-        null=False
+        null=False,
+        verbose_name=_('Table')
     )
     user = models.ForeignKey(
         to='auth.User',
         on_delete=models.DO_NOTHING,
         db_column='id_user',
-        null=False
+        null=False,
+        verbose_name=_('User')
     )
     status = models.CharField(
         null=False,
         max_length=1,
         default=Status.OPENED,
-        choices=Status.choices
+        choices=Status.choices,
+        verbose_name=_('Status')
     )
 
     class Meta:
         db_table = 'order'
+        verbose_name = _('Order')
+        verbose_name_plural = _('Orders')
+
+    def __str__(self):
+        return '{} - {}'.format(self.id, self.date)
 
 
 class OrderItem(ModelBase):
@@ -125,27 +153,36 @@ class OrderItem(ModelBase):
         to='Order',
         on_delete=models.DO_NOTHING,
         db_column='id_order',
-        null=False
+        null=False,
+        verbose_name=_('Order')
     )
     product = models.ForeignKey(
         to='Product',
         on_delete=models.DO_NOTHING,
         db_column='id_product',
-        null=False
+        null=False,
+        verbose_name=_('Product')
     )
     quantity = models.DecimalField(
         null=False,
         max_digits=10,
-        decimal_places=3
+        decimal_places=3,
+        verbose_name=_('Quantity')
     )
     unit_price = models.DecimalField(
         null=False,
         max_digits=10,
-        decimal_places=2
+        decimal_places=2,
+        verbose_name=_('Unit price')
     )
 
     class Meta:
         db_table = 'order_item'
+        verbose_name = _('Order item')
+        verbose_name_plural = _('Order items')
+
+    def __str__(self):
+        return '{} - {}'.format(self.order, self.product.name)
 
 
 class MeansPaymentOrder(ModelBase):
@@ -153,19 +190,27 @@ class MeansPaymentOrder(ModelBase):
         to='Order',
         on_delete=models.DO_NOTHING,
         db_column='id_order',
-        null=False
+        null=False,
+        verbose_name=_('Order')
     )
     means_payment = models.ForeignKey(
         to='MeansPayment',
         on_delete=models.DO_NOTHING,
         db_column='id_means_payment',
-        null=False
+        null=False,
+        verbose_name=_('Means payment')
     )
     value = models.DecimalField(
         null=False,
         max_digits=10,
-        decimal_places=2
+        decimal_places=2,
+        verbose_name=_('Value')
     )
 
     class Meta:
         db_table = 'means_payment_order'
+        verbose_name = _('Means payment order')
+        verbose_name_plural = _('Means payment orders')
+
+    def __str__(self):
+        return '{} - {}'.format(self.order, self.means_payment.description)
